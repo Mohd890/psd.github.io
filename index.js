e018f2b715aec9997a02a47f8eebde30d157aaeb
Mohd890 <mma.1985@icloud.com>
2025-02-23 19:26:44 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import supabase from './supabase.js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://mftwuovfzlooimevxrmz.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mdHd1b3Zmemxvb2ltZXZ4cm16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwODA1NDQsImV4cCI6MjA1NTY1NjU0NH0.bBeE3_OtfZDkzyCGKfrl77oN9xY7FJjXJ4nEiyptdOg';
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // تعريف عناصر الصفحة
 const loginForm = document.getElementById('loginForm');
